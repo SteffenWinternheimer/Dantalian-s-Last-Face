9652e5616e980ee7c9679c9fbd5c626e9e51943b
Steffen Winternheimer <steffen.winternheimer@gmail.com>
2021-10-09 11:00:52 +0200

    Add Levels Add Spawning Systems Add Party Members

---
+++ Dantalian's Last Face/Assets/PartyMember.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PartyMember : MonoBehaviour
+{
+    int id;
+    string name;
+    bool isFlipped;
+    PartyMember(int id, string name, bool isFlipped)
+    {
+        this.id = id;
+        this.name = name;
+        this.isFlipped = isFlipped;
+    }
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Dantalian's Last Face/Assets/PartyMembers.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PartyMembers : MonoBehaviour
+{
+
+    public GameObject[] partyMembers;
+    public PartySpawner partySpawner;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Spawn each party member at a given position
+        for(int i = 0; i < partyMembers.Length; i++)
+        {
+            Instantiate(partyMembers[i], partySpawner.spawnPositions[i]);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
